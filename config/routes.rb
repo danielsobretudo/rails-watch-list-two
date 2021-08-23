6538615b89416e97757aedaeb7d9d0b62ad18bfe
Daniel Castro <danielsobretudo@yahoo.com.br>
2021-08-23 17:02:11 -0300

    add lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, except: [:edit, :update, :destroy]
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, except: [:new, :create]
+  end
+  resources :bookmarks, only [:destroy]
 end
